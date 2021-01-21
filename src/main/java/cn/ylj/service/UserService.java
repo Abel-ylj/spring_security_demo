e9d9e27afd13091f501797626813826742a6fdd2
ylj <aboutlistener@163.com>
2021-01-21 18:00:02 +0800

    自定义方法，将验证逻辑植入框架(注入密码加密器)

--- src/main/java/cn/ylj/service/UserService.java
+++ src/main/java/cn/ylj/service/UserService.java
@@ -2,13 +2,16 @@ package cn.ylj.service;
 
 import cn.ylj.entity.UserEntity;
 import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
 
 /**
@@ -22,11 +25,14 @@ import java.util.HashMap;
 
 public class UserService implements UserDetailsService {
 
+    @Resource
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         //根据前端传过来的用户名，封装框架要的User对象，给框架，让它去校验密码
-        UserEntity user = this.findUserInDb(username);
+//        UserEntity user = this.findUserInDb(username);
+        UserEntity user = this.findUserEncryptedInDb(username);
         if (user != null){
             if (user.getUsername().equals("admin")){
                 return new User(username, user.getPassword(), Lists.newArrayList(new SimpleGrantedAuthority("ROLE_ADMIN")));
@@ -36,6 +42,24 @@ public class UserService implements UserDetailsService {
         return null;
     }
 
+    /**
+     * 用密文密码让框架校验
+     * @param username
+     * @return
+     */
+    private UserEntity findUserEncryptedInDb(String username){
+        HashMap<String, UserEntity> map = new HashMap<>();
+        map.put("jack",new UserEntity("jack",bCryptPasswordEncoder.encode("123456")));
+        map.put("rose",new UserEntity("rose", bCryptPasswordEncoder.encode("654321")));
+        map.put("admin",new UserEntity("admin",bCryptPasswordEncoder.encode("qwerty")));
+        return map.get(username);
+    }
+
+    /**
+     * 让框架用明文密码校验{noop}前缀修饰
+     * @param username
+     * @return
+     */
     private UserEntity findUserInDb(String username) {
         HashMap<String, UserEntity> map = new HashMap<>();
         map.put("jack",new UserEntity("jack","{noop}123456"));
